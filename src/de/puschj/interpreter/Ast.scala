cb544f6e478e1004c0360392a1efa1c59c65f767
Jonas Pusch <puschj@mathematik.uni-marburg.de>
2012-04-26 11:16:13 +0200

    changed AST classes according to Parser changes

--- src/de/puschj/interpreter/Ast.scala
+++ src/de/puschj/interpreter/Ast.scala
@@ -1,5 +1,7 @@
 package de.puschj.interpreter
 
+import de.fosd.typechef.conditional._
+
 
 trait Visitor {
   def visit(node: ASTNode)
@@ -9,19 +11,19 @@ abstract class ASTNode {
   def accept(visitor: Visitor) = visitor.visit(this)
 }
 
-class Program(stmts: List[Statement]) {
-  def run(env: Environment) = stmts foreach (StatementExecutor.execute(_, env))
+case class Program(stmts: List[Opt[Statement]]) {
+  //def run(env: Environment) = stmts foreach (StatementExecutor.execute(_, env))
   def print() = println(stmts)
 }
 
 sealed abstract class Statement extends ASTNode
 case class Assignment(name: String, value: Expression) extends Statement
-case class Block(stmts: List[Statement]) extends Statement
+case class Block(stmts: List[Opt[Statement]]) extends Statement
 case class While(cond: Condition, stmt: Statement) extends Statement
 case class If(cond: Condition, s1: Statement, s2: Option[Statement]) extends Statement
+case class Assert(cond: Condition) extends Statement
 
-
-sealed abstract class Expression
+sealed abstract class Expression extends Statement
 case class Num(n: Int) extends Expression
 case class Add(e1: Expression, e2: Expression) extends Expression
 case class Sub(e1: Expression, e2: Expression) extends Expression
@@ -31,7 +33,8 @@ case class Id(x: String) extends Expression
 case class Parens(e: Expression) extends Expression
 
 
-sealed abstract class Condition
+sealed abstract class Condition extends ASTNode
+case class Equal(e1: Expression, e2: Expression) extends Condition
 case class GreaterThan(e1: Expression, e2: Expression) extends Condition
 case class LessThan(e1: Expression, e2: Expression) extends Condition
 case class GreaterOrEqualThan(e1: Expression, e2: Expression) extends Condition
