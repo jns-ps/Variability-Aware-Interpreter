4bb5499fb921053ea35420574c394d1bb1b2ce15
Jonas Pusch <puschj@mathematik.uni-marburg.de>
2012-04-26 10:43:11 +0200

    introduced MultiParser

--- src/de/puschj/parser/Parsers.scala
+++ src/de/puschj/parser/Parsers.scala
@@ -3,65 +3,103 @@ import scala.util.parsing.combinator.JavaTokenParsers
 import scala.util.parsing.combinator.Parsers
 import de.puschj.interpreter._
 import scala.util.parsing.input.CharSequenceReader
+import de.fosd.typechef.featureexpr.FeatureExpr
+import de.fosd.typechef.parser._
+import de.fosd.typechef.conditional.{One, Choice, Conditional, Opt}
+import de.fosd.typechef.parser.MultiFeatureParser
+import de.fosd.typechef.parser.java15.TokenWrapper
+import de.fosd.typechef.parser.java15.JavaLexer
+import de.fosd.typechef.featureexpr.FeatureExpr
+import de.fosd.typechef.featureexpr.FeatureExprFactory
 
 
-
-class StatementParser extends ConditionParser {
+class WhileParser extends MultiFeatureParser() {
+    type Elem = TokenWrapper
+    type TypeContext = Null
+    
+    def identifier: MultiParser[Elem] = token("identifier", x=>x.getText().matches("""[a-zA-Z]([a-zA-Z0-9]|_[a-zA-Z0-9])*"""))
+    
+    def textToken(t: String): MultiParser[Elem] =
+      token(t, _.getText == t)  
+      
+    def intToken : MultiParser[Elem] =
+      token("integer", x=>x.getText().matches("""([1-9][0-9]*)|0"""))
+    
+    lazy val start: MultiParser[List[Opt[Statement]]] = textToken("begin") ~> repOpt(statement) <~ textToken("end")
   
-    lazy val prgm : Parser[Program] = (stmt+) ^^ { case s => new Program(s) }
+    // Statement
+    lazy val statement: MultiParser[Statement] = assignStatement | whileStatement | blockStatement | ifStatement | assertStatement
     
-    lazy val assignStmt : Parser[Statement] = ident ~ "=" ~ expr ~ ";" ^^ {
-      case i~_~e~_ => Assignment(i.toString(), e)
+    lazy val assignStatement : MultiParser[Statement] = identifier ~ textToken("=") ~ expression ~ textToken(";") ^^ {
+      case x~_~ e~_ => new Assignment(x.getText(), e)
     }
-    
-    lazy val blockStmt : Parser[Statement] = "{" ~> rep(stmt) <~ "}" ^^ { case l => Block(l) }
-    
-    lazy val whileStmt : Parser[Statement] = "while" ~> ("(" ~> cond <~ ")") ~ stmt ^^ { case c~s => While(c,s) }
-    
-    lazy val ifStmt : Parser[Statement] = "if" ~> ("(" ~> cond <~ ")") ~ stmt ~ (("else" ~> stmt)?) ^^ {
+    lazy val blockStatement : MultiParser[Statement] = textToken("{") ~> repOpt(statement) <~ textToken("}") ^^ { 
+      case l => Block(l) 
+    }
+    lazy val whileStatement : MultiParser[Statement] = textToken("while") ~> (textToken("(") ~> condition <~ textToken(")")) ~ statement ^^ { 
+      case c~s => While(c,s) 
+    }
+    lazy val ifStatement : MultiParser[Statement] = textToken("if") ~> (textToken("(") ~> condition <~ textToken(")")) ~ statement ~ ((textToken("else") ~> statement)?) ^^ {
       case c~s1~s2 => If(c,s1,s2)
     } 
-    
-    lazy val stmt = assignStmt | whileStmt | blockStmt | ifStmt
-    
-    def parse(s: String) = phrase(prgm)(new CharSequenceReader(s))
-    
-	def apply(s: String): Program = {
-		parse(s) match {
-		    case Success(tree, _) => tree
-		    case e: NoSuccess =>
-		           throw new IllegalArgumentException("Bad syntax: "+s)
-		}
+    lazy val assertStatement : MultiParser[Statement] = textToken("assert") ~> textToken("(") ~> condition  <~ textToken(")") <~ textToken(";") ^^ {
+      c => Assert(c)
     }
-}
 
+    // Condition
+    lazy val equal: MultiParser[Condition] = expression ~ textToken("==") ~ expression ^^ { 
+      case e1~_~e2 => Equal(e1,e2)
+    }
+    lazy val greater: MultiParser[Condition] = expression ~ textToken(">") ~ expression ^^ { 
+      case e1~_~e2 => GreaterThan(e1,e2)
+    }
+    lazy val less: MultiParser[Condition] = expression ~ textToken("<") ~ expression ^^ {
+      case e1~_~e2 => LessThan(e1,e2)
+    }
+    lazy val greaterOrEqual: MultiParser[Condition] = expression ~ textToken(">=") ~ expression ^^ {
+      case e1~_~e2 => GreaterOrEqualThan(e1,e2)
+    }
+    lazy val lessOrEqual: MultiParser[Condition] = expression ~ textToken("<=") ~ expression ^^ {
+      case e1~_~e2 => LessOrEqualThan(e1,e2)
+    }
+    lazy val negation = textToken("!") ~> condition ^^ {
+      case c => Neg(c)
+    }
+    lazy val condition : MultiParser[Condition] = equal | greater | less | greaterOrEqual | lessOrEqual
 
-class ConditionParser extends ExpressionParser {
+    // Expression
+    lazy val factor: MultiParser[Expression] = 
+      parenthesis |
+      intToken ^^ { x => new Num(Integer.parseInt(x.getText()))} |
+      identifier ^^ { x => new Id(x.getText()) }
+      
+    lazy val parenthesis : MultiParser[Expression] = textToken("(") ~> expression <~ textToken(")") ^^ {
+      e => Parens(e)
+    }
+    lazy val expression: MultiParser[Expression] = term ~ repPlain(textToken("+") ~ term | textToken("-") ~ term) ^^ reduceList
+    
+    lazy val term: MultiParser[Expression] = factor ~ repPlain(textToken("*") ~ factor | textToken("/") ~ factor) ^^ reduceList
   
-  lazy val greater = expr ~ ">" ~ expr ^^ { case e1~_~e2 => GreaterThan(e1,e2) }
-  lazy val less = expr ~ "<" ~ expr ^^ { case e1~_~e2 => LessThan(e1,e2) }
-  lazy val greaterOrEqual = expr ~ ">=" ~ expr ^^ { case e1~_~e2 => GreaterOrEqualThan(e1,e2) }
-  lazy val lessOrEqual = expr ~ "<=" ~ expr ^^ { case e1~_~e2 => LessOrEqualThan(e1,e2) }
+    val reduceList: Expression ~ List[Elem ~ Expression] => Expression = {
+      case i ~ ps => (i /: ps)(reduce) 
+    }
   
-  lazy val negation = "!" ~ cond ^^ { case _~c => Neg(c) }
-  
-  lazy val cond : Parser[Condition] = greater | less | greaterOrEqual | lessOrEqual
-}
+    def reduce(l: Expression, r: Elem ~ Expression) = r._1.getText() match {
+      case "+" => Add(l, r._2)
+      case "-" => Sub(l, r._2)
+      case "*" => Mul(l, r._2)
+      case "/" => Div(l, r._2)
+    }
 
-
-
-class ExpressionParser extends JavaTokenParsers {
-
-  lazy val int = wholeNumber ^^ { s => Num(s.toInt) }
-  lazy val variable = ident ^^ { s => Id(s) }
-  lazy val parenthesis : Parser[Expression] = "(" ~> expr <~ ")" ^^ { e => Parens(e) }
-  lazy val term = (parenthesis | int | variable)
-  lazy val expr = sum | term
-  
-  lazy val sum = product * ("+" ^^^ { (a:Expression, b:Expression) => Add(a,b) } |
-                       "-" ^^^ { (a:Expression, b:Expression) => Sub(a,b) } )
-                    
-  lazy val product = term * ("*" ^^^ { (a:Expression, b:Expression) => Mul(a,b) } |
-		  			    "/" ^^^ { (a:Expression, b:Expression) => Div(a,b) } )
-
+    def runTest(code:String) = {
+      val parser = new WhileParser()
+      val y = parser.start(JavaLexer.lex(code), FeatureExprFactory.True)
+      y match{
+        case parser.Success(v,_) =>
+            val p = new Program(v)
+            println(p)
+        case e: parser.NoSuccess =>
+            throw new IllegalArgumentException("Bad syntax: "+code)
+      }
+    }
 }
