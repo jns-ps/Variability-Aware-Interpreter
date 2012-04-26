20735542e082e5739a6adfa965e2f90a464e5de6
Jonas Pusch <puschj@mathematik.uni-marburg.de>
2012-04-26 11:15:20 +0200

    created simple JUnit test case for parser

---
+++ src/de/puschj/parser/test/ParserTest.scala
@@ -0,0 +1,40 @@
+package de.puschj.parser.test
+
+import scala.util.parsing.input.CharSequenceReader
+import scala.util.parsing.combinator.lexical.StdLexical
+import scala.io.Source
+import de.puschj.parser.WhileParser
+import de.puschj.interpreter.Statement
+import de.puschj.interpreter.Environment
+import de.fosd.typechef.conditional._
+import org.junit.Test
+import org.junit.Before
+
+class ParserTest {
+  
+  val parser = new WhileParser()
+
+  @Test
+  def whileTest() {
+      val source = scala.io.Source.fromFile("program_while.txt")
+      val input = source.mkString
+      source.close()
+      parser.runTest(input)
+  }
+  
+  @Test
+  def ifTest() {
+      val source = scala.io.Source.fromFile("program_if.txt")
+      val input = source.mkString
+      source.close()
+      parser.runTest(input)
+  }
+    
+  @Test
+  def expressionTest() {
+      val source = scala.io.Source.fromFile("program_expression.txt")
+      val input = source.mkString
+      source.close()
+      parser.runTest(input)
+  }
+}
