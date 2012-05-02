d9e38d02daf5f1e195fce343bb7484bc76fadf38
Jonas Pusch <puschj@mathematik.uni-marburg.de>
2012-05-02 15:17:15 +0200

    Environment without ConditionalMap

--- src/de/puschj/interpreter/Environment.scala
+++ src/de/puschj/interpreter/Environment.scala
@@ -1,12 +1,28 @@
 package de.puschj.interpreter
 
-import de.fosd.typechef.conditional.ConditionalMap
+import scala.collection.mutable.Map
+import de.fosd.typechef.featureexpr.FeatureExpr
+import de.fosd.typechef.conditional.Conditional
+import scala.collection.mutable.HashMap
 
-class Environment extends ConditionalMap[String,Int] {
+class Environment {
+  
+  val entries: Map[String,Conditional[Int]] = new HashMap[String,Conditional[Int]]()
 
   def print() {
     println("====== Environment =======")
-    println(toString())
+    println(entries.toString())
     println("==========================")
+  }
+  
+  def put(key: String, value: Conditional[Int]): Unit = {
+    entries.put(key, value)
+  }
+  
+  @throws(classOf[NoSuchElementException])
+  def get(key: String): Conditional[Int] = {
+    if (!entries.contains(key))
+      throw new NoSuchElementException("no value stored for "+key)
+    return entries.get(key).get
   }
 }
