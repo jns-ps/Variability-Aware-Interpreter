5d16bc50fec71816740fe156157c404357e3d0ce
puschj <jonaspusch@arcor.de>
2012-06-06 15:34:24 +0200

    adjusted pretty printing for blocks

--- src/de/puschj/interpreter/PrettyPrinter.scala
+++ src/de/puschj/interpreter/PrettyPrinter.scala
@@ -33,8 +33,8 @@ object ASTPrettyPrinter {
       case Block(stmts) => {
         if (stmts.isEmpty) Empty
         else {
-	       var doc: Doc = stmts(0).entry
-	       for (optStmt <- stmts.tail) {
+	       var doc: Doc = Empty
+	       for (optStmt <- stmts) {
 	         doc = doc ~ prettyPrintFeatureExpr(optStmt.feature) <~ prettyPrintNode(optStmt.entry) ~ prettyPrintFeatureExprClose(optStmt.feature)
 	       }
 	       doc
