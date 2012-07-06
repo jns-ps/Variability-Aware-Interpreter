25d1211c8c7ff3b2329e3a5c2617d4e8cf65eca1
puschj <jonaspusch@arcor.de>
2012-07-06 15:54:58 +0200

    added statistic functions for programs

--- src/de/puschj/interpreter/ProgramUtils.scala
+++ src/de/puschj/interpreter/ProgramUtils.scala
@@ -1,10 +1,16 @@
 package de.puschj.interpreter
+
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.{Map => MMap}
+import scala.collection.mutable.{Set => MSet}
+import scala.collection.mutable.{HashSet => MHashSet}
 import de.puschj.parser.WhileParser
 import de.fosd.typechef.featureexpr.FeatureExprFactory
 import de.fosd.typechef.featureexpr.FeatureExprFactory._
 import de.fosd.typechef.conditional.ConditionalLib
+import java.text.SimpleDateFormat
+import de.puschj.interpreter.benchmark.BenchmarkResult
+import de.fosd.typechef.conditional.Opt
 
 
 object ProgramUtils {
@@ -18,7 +24,66 @@ object ProgramUtils {
       result.toList
     }
     
-    private def allProgramVariants(program: VariableProgram, availableFeatures: Set[String]) = {
+    def countProgramStatments(program: VariableProgram) = countStatements(program.getStatements)
+    
+    private def countStatements(stmts: List[Opt[Statement]]): Int = {
+      var c = 0
+      for (optStmt <- stmts) {
+        val stmt = optStmt.entry
+        stmt match {
+          case Block(body) => c += countStatements(body)
+          case While(_, Block(body)) => c += (countStatements(body) + 1)
+          case If(_, Block(thn), None) => c += (countStatements(thn) + 1)
+          case If(_, Block(thn), Some(Block(els))) => c += (countStatements(thn) + countStatements(els) + 1) 
+          case s => c += 1
+        }
+      }
+      return c
+    }
+    
+    def countProgramFeatures(program: VariableProgram) = countFeatures(program.getStatements)
+    
+    private def countFeatures(stmts: List[Opt[Statement]]) = {
+      val features = distinctFeatures(stmts)
+      features.size
+    }
+    
+    private def distinctFeatures(stmts: List[Opt[Statement]]): MSet[String] = {
+      var features = MHashSet.empty[String]
+      for (optStmt <- stmts) {
+        features ++= optStmt.feature.collectDistinctFeatures
+        val stmt = optStmt.entry
+        stmt match {
+          case Block(body) => features ++= distinctFeatures(body)
+          case While(_, Block(body)) => features ++= distinctFeatures(body)
+          case If(_, Block(thn), None) => features ++= distinctFeatures(thn)
+          case If(_, Block(thn), Some(Block(els))) => features ++= distinctFeatures(thn) ++= distinctFeatures(els)
+          case s => 
+        }      
+      }
+      features
+    }
+    
+    def countProgramFeatureExpressions(program: VariableProgram) = countFeatures(program.getStatements)
+    
+    private def countFeatureExpressions(stmts: List[Opt[Statement]]): Int = {
+      var features = 0
+      for (optStmt <- stmts) {
+        val feat = optStmt.feature
+        val stmt = optStmt.entry
+        stmt match {
+          case Block(body) => features += countFeatures(body)
+          case While(_, Block(body)) => features += countFeatures(body)
+          case If(_, Block(thn), None) => features += countFeatures(thn)
+          case If(_, Block(thn), Some(Block(els))) => features += (countFeatures(thn) + countFeatures(els))
+          case s => 
+        }
+        if (feat != True) features += 1
+      }
+      features
+    }
+    
+    def allProgramVariants(program: VariableProgram, availableFeatures: Set[String]) = {
         val mmap = MMap.empty[ConfiguredProgram, List[Set[String]]]
         for (selectedFeatures <- allCombinations(availableFeatures.toList)) {
           val configuredProgram = program.configured(selectedFeatures)
@@ -33,9 +98,8 @@ object ProgramUtils {
     
     def compareProgramVariants(program: VariableProgram, availableFeatures: Set[String], comparedVariables: Set[String]): Boolean = {
       val variableStore = program.run(new Store, new FuncStore)
-      val configuredStores = MMap.empty[Store, List[Set[String]]]
-      
-      for (programVariant <- allProgramVariants(program, availableFeatures)) {
+      val allVariantsMap = allProgramVariants(program, availableFeatures)
+      for (programVariant <- allVariantsMap) {
         val configuredStore = programVariant._1.run(new Store, new FuncStore)
         for (selectedFeatures <- programVariant._2) {
            var context = True
@@ -44,7 +108,6 @@ object ProgramUtils {
            for (variable <- comparedVariables) {
              val varStoreValue = variableStore.getByContext(variable, context)
              val cnfStoreValue = configuredStore.get(variable)
-//             println("comparing "+varStoreValue+"/"+cnfStoreValue)
              if (!ConditionalLib.equals(varStoreValue, cnfStoreValue)) {
                System.err.println("Different Values for \""+variable+"\" - expected: "+cnfStoreValue+" result: "+varStoreValue);
 //               System.err.println("==== Variable Program: ====\n"+program.toString+"\n");
@@ -56,4 +119,5 @@ object ProgramUtils {
       }
       return true
     }
+    
 }
